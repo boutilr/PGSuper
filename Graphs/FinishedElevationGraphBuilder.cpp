ed6535840775fea77684aa848672be8e233fd6fc
rdpBridgeSight <44646153+rdpBridgeSight@users.noreply.github.com>
2023-01-05 08:12:12 -0800

    small fixes

--- Graphs/FinishedElevationGraphBuilder.cpp
+++ Graphs/FinishedElevationGraphBuilder.cpp
@@ -127,16 +127,21 @@ void CFinishedElevationGraphBuilder::UpdateYAxis()
    m_Graph.PinYAxisAtZero(false);
 
    GET_IFACE(IEAFDisplayUnits,pDisplayUnits);
-   const WBFL::Units::LengthData& lengthUnit = pDisplayUnits->GetSpanLengthUnit();
-   m_pYFormat = new WBFL::Units::LengthTool(lengthUnit);
-   m_Graph.SetYAxisValueFormat(*m_pYFormat);
 
    if (gtElevationDifferential == m_GraphType)
    {
+      const WBFL::Units::LengthData& lengthUnit = pDisplayUnits->GetComponentDimUnit();
+      m_pYFormat = new WBFL::Units::LengthTool(lengthUnit);
+      m_Graph.SetYAxisValueFormat(*m_pYFormat);
+
       m_Graph.SetYAxisTitle(std::_tstring(_T("Elevation Differential (") + m_pYFormat->UnitTag() + _T(")")).c_str());
    }
    else
    {
+      const WBFL::Units::LengthData& lengthUnit = pDisplayUnits->GetSpanLengthUnit();
+      m_pYFormat = new WBFL::Units::LengthTool(lengthUnit);
+      m_Graph.SetYAxisValueFormat(*m_pYFormat);
+
       m_Graph.SetYAxisTitle(std::_tstring(_T("Elevation (") + m_pYFormat->UnitTag() + _T(")")).c_str());
    }
 }
