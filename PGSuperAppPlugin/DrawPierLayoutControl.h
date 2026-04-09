d3c96bf48e13bf142e79fedb54d224b404067f2e
boutilr <115045413+boutilr@users.noreply.github.com>
2026-04-09 09:43:49 -0700

    starter code for dynamic dimensioning

--- PGSuperAppPlugin/DrawPierLayoutControl.h
+++ PGSuperAppPlugin/DrawPierLayoutControl.h
@@ -42,12 +42,22 @@ public:
 
 	void CustomInit(IPierLayoutDataSource* pSource);
 
+protected:
 	afx_msg void OnPaint();
 	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
 
-protected:
 	DECLARE_MESSAGE_MAP()
+
+private:
 	IPierLayoutDataSource* m_pSource;
 
 	void DrawPierGeometry(CDC* pDC, WBFL::Graphing::PointMapper& mapper);
+
+	void DrawSymbolicDimensions(CDC* pDC, WBFL::Graphing::PointMapper& mapper,
+		Float64 h_left, Float64 h2_left, Float64 x1_left, Float64 x2_left,
+		Float64 h_right, Float64 h2_right, Float64 x1_right, Float64 x2_right);
+	void DrawVerticalDimension(CDC* pDC, WBFL::Graphing::PointMapper& mapper,
+		Float64 x, Float64 y1, Float64 y2, LPCTSTR pszLabel);
+	void DrawHorizontalDimension(CDC* pDC, WBFL::Graphing::PointMapper& mapper,
+		Float64 x1, Float64 y, Float64 x2, LPCTSTR pszLabel);
 };
