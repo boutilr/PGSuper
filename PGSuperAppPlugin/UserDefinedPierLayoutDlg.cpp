33065fb4d6ed4c58437326a60fec4a5585dd1d69
boutilr <115045413+boutilr@users.noreply.github.com>
2026-08-11 16:01:05 -0700

    removes draw control part 1

--- PGSuperAppPlugin/UserDefinedPierLayoutDlg.cpp
+++ PGSuperAppPlugin/UserDefinedPierLayoutDlg.cpp
@@ -85,74 +85,12 @@ BEGIN_MESSAGE_MAP(CUserDefinedPierLayoutDlg, CDialog)
 
 END_MESSAGE_MAP()
 
-// Add these handler implementations at the end of the file
-void CUserDefinedPierLayoutDlg::OnLButtonDown(UINT nFlags, CPoint point)
-{
-    CRect rcControl;
-    m_ctrlDrawXBeam.GetWindowRect(&rcControl);
-    ScreenToClient(&rcControl);
-
-    if (rcControl.PtInRect(point))
-    {
-        m_ctrlDrawXBeam.SendMessage(WM_LBUTTONDOWN, nFlags, MAKELPARAM(point.x - rcControl.left, point.y - rcControl.top));
-        return;
-    }
-
-    CDialog::OnLButtonDown(nFlags, point);
-}
-
-void CUserDefinedPierLayoutDlg::OnLButtonUp(UINT nFlags, CPoint point)
-{
-    CRect rcControl;
-    m_ctrlDrawXBeam.GetWindowRect(&rcControl);
-    ScreenToClient(&rcControl);
-
-    if (rcControl.PtInRect(point))
-    {
-        m_ctrlDrawXBeam.SendMessage(WM_LBUTTONUP, nFlags, MAKELPARAM(point.x - rcControl.left, point.y - rcControl.top));
-        return;
-    }
-
-    CDialog::OnLButtonUp(nFlags, point);
-}
-
-void CUserDefinedPierLayoutDlg::OnMouseMove(UINT nFlags, CPoint point)
-{
-    CRect rcControl;
-    m_ctrlDrawXBeam.GetWindowRect(&rcControl);
-    ScreenToClient(&rcControl);
-
-    if (rcControl.PtInRect(point))
-    {
-        m_ctrlDrawXBeam.SendMessage(WM_MOUSEMOVE, nFlags, MAKELPARAM(point.x - rcControl.left, point.y - rcControl.top));
-        return;
-    }
-
-    CDialog::OnMouseMove(nFlags, point);
-}
-
-void CUserDefinedPierLayoutDlg::OnLButtonDblClk(UINT nFlags, CPoint point)
-{
-    CRect rcControl;
-    m_ctrlDrawXBeam.GetWindowRect(&rcControl);
-    ScreenToClient(&rcControl);
-
-    if (rcControl.PtInRect(point))
-    {
-        m_ctrlDrawXBeam.SendMessage(WM_LBUTTONDBLCLK, nFlags, MAKELPARAM(point.x - rcControl.left, point.y - rcControl.top));
-        return;
-    }
-
-    CDialog::OnLButtonDblClk(nFlags, point);
-}
 
 /////////////////////////////////////////////////////////////////////////////
 // CUserDefinedPierLayoutDlg message handlers
 
 BOOL CUserDefinedPierLayoutDlg::OnInitDialog()
 {
-    m_ctrlDrawXBeam.SubclassDlgItem(IDC_PIER_LAYOUT, this);
-    m_ctrlDrawXBeam.CustomInit(this);
 
     m_ColumnLayoutGrid.SubclassDlgItem(IDC_COLUMN_GRID, this);
     m_ColumnLayoutGrid.CustomInit();
@@ -234,16 +172,6 @@ void CUserDefinedPierLayoutDlg::DoDataExchange(CDataExchange* pDX)
 
 void CUserDefinedPierLayoutDlg::RefreshDisplay()
 {
-    // Invalidate and update the drawing control to reflect the changes
-    CPierLayoutPage* pPage =
-        DYNAMIC_DOWNCAST(CPierLayoutPage, GetParent());
-    if (pPage->m_pPierLayoutPopout != nullptr)
-    {
-        m_ctrlDrawXBeam.UpdateDisplayObjects();
-        m_ctrlDrawXBeam.Invalidate();
-        m_ctrlDrawXBeam.UpdateWindow();
-    }
-
     if (GetParent())
         GetParent()->SendMessage(WM_PIER_LAYOUT_CHANGED);
 }
