04a70d92713734d5903eb0a74a0e63ee810184ea
boutilr <115045413+boutilr@users.noreply.github.com>
2026-08-12 15:38:56 -0700

    fixes not saving column offset

--- PGSuperAppPlugin/ScallopedPierLayoutDlg.cpp
+++ PGSuperAppPlugin/ScallopedPierLayoutDlg.cpp
@@ -217,7 +217,6 @@ void CScallopedPierLayoutDlg::FillRefColumnComboBox(ColumnIndexType nColumns)
     if (pcbRefColumn->SetCurSel(curSel) == CB_ERR)
     {
         pcbRefColumn->SetCurSel(m_RefColumnIdx);
-        OnRefColumnChanged();
     }
 }
 
